388527d29f4674094559da1734df4c14449d96f6
mytcer <woaibou@163.com>
2015-01-12 18:27:41 +0800

    更新联系人demo

--- UIKit/UITableView/ContactDemo/ContactDemo/ViewController.m
+++ UIKit/UITableView/ContactDemo/ContactDemo/ViewController.m
@@ -10,31 +10,37 @@
 #import "Contact.h"
 #import "ContactGroup.h"
 
+#define STATUSBAR_HEIGHT 20
+#define TOP_TOOLBAR_HEIGHT 44
+#define SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
+#define SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
+
 @interface ViewController ()
 
-#pragma mark UITableView实例
 @property UITableView *tableview;
-
-#pragma mark 联系人数据集合
 @property NSMutableArray *contacts;
-
-#pragma mark 当前被选中的行
 @property NSIndexPath *selectedIndexPath;
+@property BOOL isDeleteContact;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - ViewController 生命周期方法
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initContactData];
-    self.tableview = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
-    self.tableview.dataSource = self;
-    self.tableview.delegate = self;
-    [self.view addSubview:self.tableview];
+    [self initToolbar];
+    [self initTableView];
 }
 
-#pragma mark 初始化联系人数据
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 私有方法
+// 初始化联系人数据
 - (void)initContactData {
     self.contacts = [[NSMutableArray alloc] init];
     
@@ -50,19 +56,51 @@
     [self.contacts addObject:group2];
 }
 
+// 初始化表格
+- (void)initTableView {
+    self.tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, STATUSBAR_HEIGHT + TOP_TOOLBAR_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - STATUSBAR_HEIGHT - TOP_TOOLBAR_HEIGHT) style:UITableViewStyleGrouped];
+    self.tableview.dataSource = self;
+    self.tableview.delegate = self;
+    [self.view addSubview:self.tableview];
+}
+
+// 添加工具栏
+- (void)initToolbar {
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, STATUSBAR_HEIGHT, SCREEN_WIDTH, TOP_TOOLBAR_HEIGHT)];
+    
+    UIBarButtonItem *delBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(removeContact)];
+    UIBarButtonItem *spaceBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    UIBarButtonItem *addBtn = [[UIBarButtonItem alloc ] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addContact)];
+    toolbar.items = @[delBtn, spaceBtn, addBtn];
+    
+    [self.view addSubview:toolbar];
+}
+
+// 删除联系人
+- (void)removeContact {
+    self.isDeleteContact = YES;
+    [self.tableview setEditing:!self.tableview.editing animated:YES];
+}
+
+// 添加联系人
+- (void)addContact {
+    self.isDeleteContact = NO;
+    [self.tableview setEditing:!self.tableview.editing animated:YES];
+}
+
 #pragma mark - table view 数据源方法
-#pragma mark 分组数量
+// 分组数量
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return self.contacts.count;
 }
 
-#pragma mark 每组行数
+// 每组行数
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     ContactGroup *group = self.contacts[section];
     return group.contacts.count;
 }
 
-#pragma mark 表格右侧的标题索引
+// 表格右侧的标题索引
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
     NSMutableArray *indexs = [[NSMutableArray alloc] init];
     
@@ -73,7 +111,7 @@
     return indexs;
 }
 
-#pragma mark 每行的单元格
+// 每行的单元格
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *reuseIndentifier = @"Cell";
     static NSString *reuseSwitchIndentifier = @"SwitchCell";
@@ -101,35 +139,35 @@
     return cell;
 }
 
-#pragma mark 分组标题
+// 分组标题
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     ContactGroup *group = self.contacts[section];
     return group.name;
 }
 
-#pragma mark 分组脚注
+// 分组脚注
 - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
     ContactGroup *group = self.contacts[section];
     return group.detail;
 }
 
 #pragma mark - table view 代理方法
-#pragma mark 设置分组标题高度
+// 设置分组标题高度
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     return 50;
 }
 
-#pragma mark 设置分组脚注高度
+// 设置分组脚注高度
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
     return 50;
 }
 
-#pragma mark 设置每行的高度
+// 设置每行的高度
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 40;
+    return 60;
 }
 
-#pragma mark 点击行
+// 点击行
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     self.selectedIndexPath = indexPath;
     
@@ -157,9 +195,48 @@
     [self presentViewController:alert animated:YES completion:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 取得当前操作状态
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return self.isDeleteContact ? UITableViewCellEditingStyleDelete : UITableViewCellEditingStyleInsert;
+}
+
+// 实现了此方法，向左滑时就会显示删除或添加图标
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    ContactGroup *group = self.contacts[indexPath.section];
+    Contact *contact = group.contacts[indexPath.row];
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [group.contacts removeObject:contact];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];
+        
+        if (group.contacts.count == 0) {
+            [self.contacts removeObject:group];
+            [tableView reloadData];
+        }
+    } else {
+        Contact *newcontact = [[Contact alloc] init];
+        newcontact.firstName = @"周";
+        newcontact.lastName = @"八";
+        newcontact.phoneNumber = @"3456789";
+        [group.contacts insertObject:newcontact atIndex:indexPath.row];
+        [tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];
+    }
+}
+
+// 只要实现了该方法，当tableview处于编辑状态时，就可以进行排序
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+    ContactGroup *sourceGroup = self.contacts[sourceIndexPath.section];
+    Contact *sourceContact = sourceGroup.contacts[sourceIndexPath.row];
+    
+    ContactGroup *destGroup = self.contacts[destinationIndexPath.section];
+    
+    [sourceGroup.contacts removeObject:sourceContact];
+    if (sourceGroup.contacts.count == 0) {
+        [self.contacts removeObject:sourceGroup];
+        [tableView reloadData];
+    }
+    
+    [destGroup.contacts insertObject:sourceContact atIndex:destinationIndexPath.row];
 }
 
 @end
